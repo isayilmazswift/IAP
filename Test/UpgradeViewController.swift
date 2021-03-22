06748663250b9b4c90e6db9e3f96ca0b6947d101
isa yilmaz <jesus.codes@icloud.com>
2021-03-22 10:35:56 +0300

    first commit

---
+++ Test/UpgradeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UpgradeViewController.swift
+//  Test
+//
+//  Created by İsa Yılmaz on 20.03.2021.
+//
+
+import UIKit
+
+class UpgradeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
