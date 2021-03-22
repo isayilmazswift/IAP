532a3e2a0cb0a67c7aaf8716ad620a4bcca312b4
isa yilmaz <jesus.codes@icloud.com>
2021-03-22 10:38:51 +0300

    Second Commit

--- Test/UpgradeViewController.swift
+++ Test/UpgradeViewController.swift
@@ -5,25 +5,91 @@
 //  Created by İsa Yılmaz on 20.03.2021.
 //
 
+import StoreKit
 import UIKit
 
-class UpgradeViewController: UIViewController {
-
+class UpgradeViewController: UIViewController, SKPaymentTransactionObserver, SKProductsRequestDelegate {
+    
+    
+    var myPruduct: SKProduct?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        fetchProducts()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func DidTapBuy(_ sender: Any) {
+        
+        guard let myProduct = myPruduct else {return}
+        
+        if SKPaymentQueue.canMakePayments(){
+            let payment = SKPayment(product: myProduct)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+        }
+        
     }
-    */
+    
+    func fetchProducts(){
+        //com.isayilmaz.test.removeads
+        
+        let request = SKProductsRequest(productIdentifiers: ["com.isayilmaz.test.removeads"])
+        request.delegate = self
+        request.start()
+    }
+    
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
+        
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchasing:
+                // no operations
+                
+                break
+                
+            case .purchased, .restored:
+                
+                // Unlock Items
+                
+                UserDefaults.standard.set(true, forKey: "ads_removed")
+                
+                SKPaymentQueue.default().finishTransaction(transaction)
+                SKPaymentQueue.default().remove(self)
+                
+                break
+                
+            case .failed, .deferred:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                SKPaymentQueue.default().remove(self)
+                break
+                
+            default:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                SKPaymentQueue.default().remove(self)
+                break
+            }
+        }
+        
+        
+    }
+    
+}
 
+
+extension UpgradeViewController {
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        if let product = response.products.first {
+            self.myPruduct = product
+            print(product.localizedTitle)
+            print(product.localizedDescription)
+            print(product.productIdentifier)
+            print(product.price)
+        }
+    }
+    
+    
 }
--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if !UserDefaults.standard.bool(forKey: "ads_removed") {
+            // Show ads
+        }
+        
+        else {
+            //ads
+        }
+        
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func didTapRemoveAds(_ sender: Any) {
+        
+        guard let vc = storyboard?.instantiateViewController(identifier: "upgrade") else {
+            return
+        }
+        
+        vc.title = "Remove Ads"
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
 }
 
